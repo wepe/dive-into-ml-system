a6f390b6fab05ddc052b460486db536e71ffd19b
wepe <weipforever@gmail.com>
2018-06-07 13:07:01 +0800

    update readme

--- python-package/example.py
+++ python-package/example.py
@@ -1,14 +1,14 @@
 from lr import model
 import numpy as np
 
-
 features = np.load('features.dat')
 labels = np.load('labels.dat')
 
-
-clf = model(max_iter=100,alpha=0.1,l2_lambda=0.01,tolerance=0.001)
+clf = model(max_iter=10,alpha=0.1,l2_lambda=0.01,tolerance=0.001)
 clf.fit(features,labels)
-print "clf.model:",clf.fmodel
-print labels[:30]
-print clf.predict_prob(features[:30])
 print clf.predict(features[:30])
+
+clf.save("/home/wepon/lr.model")
+clf1 = model()
+clf1.load("/home/wepon/lr.model")
+print clf1.predict(features[:30])
