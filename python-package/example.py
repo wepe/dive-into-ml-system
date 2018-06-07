ee40f18067188793b83ccdb01a2a299380e80251
wepe <weipforever@gmail.com>
2018-06-07 12:52:25 +0800

    support save&load model

--- python-package/example.py
+++ python-package/example.py
@@ -6,7 +6,7 @@ features = np.load('features.dat')
 labels = np.load('labels.dat')
 
 
-clf = model(max_iter=10,alpha=0.1,l2_lambda=0.01,tolerance=0.001)
+clf = model(max_iter=100,alpha=0.1,l2_lambda=0.01,tolerance=0.001)
 clf.fit(features,labels)
 print "clf.model:",clf.fmodel
 print labels[:30]
--- python-package/lr/model.py
+++ python-package/lr/model.py
@@ -1,6 +1,7 @@
 from ctypes import *
 import numpy as np
 import os
+import shutil
 from threading import Thread
 
 liblr = cdll.LoadLibrary(os.path.dirname(os.path.realpath(__file__))+'/liblr.so')
@@ -12,51 +13,6 @@ class model(object):
         self.l2_lambda = l2_lambda
         self.tolerance = tolerance
         self.fmodel = None
-
-    # only support python list
-    def _fit(self,features,labels):
-        features = [tuple(f) for f in features]
-        row = len(features)
-        col = len(features[0])
-        # initialize ctypes array
-        double_2d_array = ((c_double*col)*row)(*features)
-        int_array = (c_int*row)(*labels)
-        del features,labels
-        # cast to C function argument's type
-        int_p = cast(int_array,POINTER(c_int))
-        double_p_list = []
-        for i in range(row):
-            double_p_list.append(cast(double_2d_array[i],POINTER(c_double)))
-        double_p_p = (POINTER(c_double)*row)(*double_p_list)
-        # call the C function
-        liblr.fit.argtypes = [POINTER(POINTER(c_double)),POINTER(c_int),c_int,c_int,c_int,c_double,c_double,c_double]
-        liblr.fit.restype = POINTER(c_char)
-        res = liblr.fit(double_p_p,int_p,c_int(row),c_int(col),c_int(self.max_iter),c_double(self.alpha),c_double(self.l2_lambda),c_double(self.tolerance))
-        self.fmodel = ''.join([res[i] for i in range(25)])
-
-    # only support python list
-    def _predict_prob(self,features):
-        assert self.fmodel is not None
-        features = [tuple(f) for f in features]
-        row = len(features)
-        col = len(features[0])
-        # initialize ctypes array
-        double_2d_array = ((c_double*col)*row)(*features)
-        del features
-        # cast to C function argument's type
-        double_p_list = []
-        for i in range(row):
-            double_p_list.append(cast(double_2d_array[i],POINTER(c_double)))
-        double_p_p = (POINTER(c_double)*row)(*double_p_list)
-        # call the C function
-        liblr.predict_prob.argtypes = [POINTER(POINTER(c_double)),c_int,c_int,POINTER(c_char)]
-        liblr.predict_prob.restype = POINTER(c_double)
-
-        #res = liblr.predict_prob(double_p_p,c_int(row),c_int(col),c_char_p(self.fmodel))
-        res = pool.apply_async(liblr.predict_prob,(double_p_p,c_int(row),c_int(col),c_char_p(self.fmodel)))
-        res = res.get()
-        return [res[i] for i in range(row)]
-
 
     # support python list, numpy array
     def fit(self,features,labels):
@@ -117,6 +73,56 @@ class model(object):
         prob = self.predict_prob(features)
         return [1 if p>0.5 else 0 for p in prob]
 
+    def save(self,path):
+        shutil.copy(self.fmodel, path)
+
+    def load(self,path):
+        self.fmodel = path
 
     def __del__(self):
         os.remove(self.fmodel)
+
+    # old version code
+    # only support python list
+    def _fit(self,features,labels):
+        features = [tuple(f) for f in features]
+        row = len(features)
+        col = len(features[0])
+        # initialize ctypes array
+        double_2d_array = ((c_double*col)*row)(*features)
+        int_array = (c_int*row)(*labels)
+        del features,labels
+        # cast to C function argument's type
+        int_p = cast(int_array,POINTER(c_int))
+        double_p_list = []
+        for i in range(row):
+            double_p_list.append(cast(double_2d_array[i],POINTER(c_double)))
+        double_p_p = (POINTER(c_double)*row)(*double_p_list)
+        # call the C function
+        liblr.fit.argtypes = [POINTER(POINTER(c_double)),POINTER(c_int),c_int,c_int,c_int,c_double,c_double,c_double]
+        liblr.fit.restype = POINTER(c_char)
+        res = liblr.fit(double_p_p,int_p,c_int(row),c_int(col),c_int(self.max_iter),c_double(self.alpha),c_double(self.l2_lambda),c_double(self.tolerance))
+        self.fmodel = ''.join([res[i] for i in range(25)])
+
+    # only support python list
+    def _predict_prob(self,features):
+        assert self.fmodel is not None
+        features = [tuple(f) for f in features]
+        row = len(features)
+        col = len(features[0])
+        # initialize ctypes array
+        double_2d_array = ((c_double*col)*row)(*features)
+        del features
+        # cast to C function argument's type
+        double_p_list = []
+        for i in range(row):
+            double_p_list.append(cast(double_2d_array[i],POINTER(c_double)))
+        double_p_p = (POINTER(c_double)*row)(*double_p_list)
+        # call the C function
+        liblr.predict_prob.argtypes = [POINTER(POINTER(c_double)),c_int,c_int,POINTER(c_char)]
+        liblr.predict_prob.restype = POINTER(c_double)
+
+        #res = liblr.predict_prob(double_p_p,c_int(row),c_int(col),c_char_p(self.fmodel))
+        res = pool.apply_async(liblr.predict_prob,(double_p_p,c_int(row),c_int(col),c_char_p(self.fmodel)))
+        res = res.get()
+        return [res[i] for i in range(row)]
